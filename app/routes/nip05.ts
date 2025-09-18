6a6b4e2b82554dd24e31474dc79b172b7951469c
Alejandro Gómez <alejandrogomez@bitrefill.com>
2025-09-18 23:03:01 +0200

    fix: cors headers

--- app/routes/nip05.ts
+++ app/routes/nip05.ts
@@ -3,5 +3,11 @@ import { getNip05 } from "~/services/data.server";
 export async function loader() {
   //const { username } = request.query;
   const response = await getNip05();
-  return Response.json(response);
+  return Response.json(response, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
 }
